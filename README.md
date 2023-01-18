73d3e88046acafa998f7d8692e09c48bf5b3243b
tpoindex <tpoindex@gmail.com>
2023-01-18 15:42:11 -0700

    Immediate/slide modes, two-step fragment handling, demo decks.

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
 # reveal-onetimer
 A [Reveal.js](https://revealjs.com/) plugin to animate fragments only once, so that
 going to a previous slide shows final fragment state(s),
-rather than tediously stepping through fragments backwards.
+rather than tediously stepping through fragments backwards and forwards again.
 
 No one wants to see the same trick twice.
 
 
 ## Installation
 
-Copy this repository into the plugin folder of your reveal.js presentation, i.e. ```plugin/onetimer```.
+Copy this repository into the plugin folder of your reveal.js presentation, i.e. ```plugin/reveal-onetimer```.
 
-Add the plugin to the Reveal initialization https://revealjs.com/of your presentation, as shown:
+Add the plugin to the Reveal initialization of your presentation, as shown:
 
 ### reveal 4.x
 
 ```javascript
-<script src="plugin/onetimer/onetimer.js"></script>
+<script src="plugin/reveal-onetimer/onetimer.js"></script>
 // .. 
 Reveal.initialize({
+        // ...
+        oneTimer: {immediateMode: true},        // optional, default is 'false'
+
 	// ...
 	plugins: [
 		// ..
@@ -25,6 +28,42 @@ Reveal.initialize({
 	]
 });
 ```
+
+## Usage
+
+OneTimer operates in one of two modes:  *slide mode* (default), or *immediate mode*.
+
+Slide mode allows fragments to re-animate by navigating backwards 
+and forwards while on the same slide.  When the end of a slide is reached, 
+OneTimer removes the "fragment" class from each fragment before proceeding
+to the next slide. After that, going backward in your deck will show the 
+final state of each fragment on the previous slide, without stepping
+backwards through each fragment's transition.  Slide mode is the default.
+
+Immediate mode removes the "fragment" class on each fragment within a slide 
+immediately when the fragment transition occurs.  
+Navigating backwards within a slide will go to the previous slide, bypassing 
+stepping through any previous fragment transitions.  Immediate mode is enabled 
+by a specifying a 'oneTimer' object in the Reveal.initalize() method, as 
+shown in the Installation section above.
+
+
+## Demo
+
+Two demo files are included (demo_slide_mode.html and demo_immediate_mode.html).  Copy
+these into the top level Reveal.js distribution, and be sure to install the onetimer.js
+plugin into the plugin/ directory.  
+
+
+## Quirks
+
+Reveal.js 4.x allows many added effects for fragments (i.e., 'grow', 'shrink', 
+'strike', 'highlight-red', etc.), which are bound to the
+"fragment" class.  Once OneTimer removes the "fragment" class, the added 
+effects are re-enabled by OneTimer.  Additional handling is needed for 'two-step'
+fragment classes.  Fragments with these extras must defer fragment cleanup until
+the next fragment cycle or slide.
+
 
 ## License
 
--- onetimer.js
+++ onetimer.js
@@ -5,23 +5,103 @@
 
 var OneTimer = window.OneTimer  ||  (function() {
 
-  var fragmentClassList = [];
+  // slide mode (default), or immediate mode if set in Reveal.initialize()
+  var immediateMode = false;
+
+  // fragment list: in slide mode, accumulate frags until next page.
+  //                in immediate mode, keep track of frags w/ special states
+  var fragmentList = [];
+
+  // list of effect style fix-ups, since removing "fragment" hinders other fragment effects
+  // 'two step' fragment effects currently not handled, see readme
+  const effects = [
+     {effect: 'fade-out',                style: {prop: 'opacity',         value: '0'            } },
+     {effect: 'semi-fade-out',           style: {prop: 'opacity',         value: '0.5'          } },
+     {effect: 'fade-in-then-out',        style: {prop: 'opacity',         value: '0'            } },
+     {effect: 'fade-in-then-semi-out',   style: {prop: 'opacity',         value: '0.5'          } },
+     {effect: 'grow',                    style: {prop: 'transform',       value: 'scale(1.3)'   } },
+     {effect: 'shrink',                  style: {prop: 'transform',       value: 'scale(0.7)'   } },
+     {effect: 'strike',                  style: {prop: 'text-decoration', value: 'line-through' } },
+     {effect: 'highlight-red',           style: {prop: 'color',           value: '#ff2c2d'      } },
+     {effect: 'highlight-blue',          style: {prop: 'color',           value: '#17ff2e'      } },
+     {effect: 'highlight-green',         style: {prop: 'color',           value: '#1b91ff'      } },
+  ]
+
+  // special handling for these fragment extras, when in immediate mode
+  // these are 'two-step' extras.
+  const special = [ 'fade-in-then-out', 'fade-in-then-semi-out', 'highlight-current-red', 'highlight-current-blue', 'highlight-current-green' ];
+
+
+  function killFragment(frag, force) {
+
+      // force == false, try to manage the two-step state
+      if (! force ) {
+	  // immediate mode special cases, kludgy state handling..
+	  // check for special state handling class, if found and not already on fragmentList, add it, and stop
+	  if (immediateMode && ! fragmentList.includes(frag)) {
+	      var found = false;
+	      special.forEach( sp => {
+		  if (frag.classList.contains(sp)) {
+		      fragmentList.push(frag);
+		      found = true;
+		  }
+	      });
+	      if (found) {
+                  // pushed the frag onto the fragmentList, next time we apply
+		  return;
+	      }
+	  } else if (immediateMode) {
+              // apply precessing to all frags on the fragmentList
+              fragmentList.forEach(frag => killFragment(frag, true));
+	  }
+      }
+
+      // continue with regular handling and fix-ups
+      frag.classList.remove("fragment");
+
+      // apply extra effects to restore the fragment text appearence
+      effects.forEach(ef => { 
+          if (frag.classList.contains(ef.effect)) {
+              frag.style[ef.style.prop] = ef.style.value;
+          }
+      });
+  }
 
   return {
       id: 'OneTimer',
       init: (deck) => {
 
+          // check if reveal config object has oneTimer.immediateMode
+          var config = Reveal.getConfig();
+          if (config && config.oneTimer && config.oneTimer.immediateMode) {
+              immediateMode = config.oneTimer.immediateMode;
+          }
+
           // each time we progress to the next slide, remove the "fragment" class from accumlated fragments.
+          // and reset the fragmentList to empty
           Reveal.on('slidechanged', event => {
               // event.previousSlide, event.currentSlide, event.indexh, event.indexv
-              fragmentClassList.forEach(cl => cl.remove("fragment"));
-              fragmentClassList = [];
+              fragmentList.forEach(frag => killFragment(frag, true));
+              fragmentList = [];
           } );
 
-          // on each fragment accumulate the fragment classList
+          // on each fragment accumulate to apply on slide change, or
+          // if immediateMode is true, remove the "fragment" class now.
           Reveal.on('fragmentshown', event => {
               // event.fragment
-              fragmentClassList.push(event.fragment.classList);   // each classList is a DOMTokenList obj
+              if (immediateMode) {
+                  killFragment(event.fragment, false);
+              } else {
+                  fragmentList.push(event.fragment);
+              }
+          } );
+
+          // if immediateMode is true, remove all "fragment" on fragmenthidden now.
+          Reveal.on('fragmenthidden', event => {
+              // event.fragment
+              if (immediateMode) {
+                  killFragment(event.fragment, true);
+              }
           } );
 
       }
